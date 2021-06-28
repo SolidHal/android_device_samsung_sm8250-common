4c0ca4ea0aa3972cd43e387cb1914159258b1762
SolidHal <hal@halemmerich.com>
2021-06-28 23:47:04 +0000

    add in some more init items

---
+++ init/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH:= $(call my-dir)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE       := init.qcom.rc.sys
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+
+include $(BUILD_PREBUILT)
