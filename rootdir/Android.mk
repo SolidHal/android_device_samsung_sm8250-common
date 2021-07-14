d4353fb4aa49dd9281aa5812600307c0433313fb
SolidHal <hal@halemmerich.com>
2021-07-14 16:14:11 +0000

    remove dupe bin media script

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -130,14 +130,6 @@ LOCAL_SRC_FILES    := bin/init.qti.fm.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qti.media.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qti.media.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
 # Common init scripts
 
 include $(CLEAR_VARS)
