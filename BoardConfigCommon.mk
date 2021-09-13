d6892b503f5b48e7d9f1454e8608d1c404a84222
SolidHal <hal@halemmerich.com>
2021-09-13 22:43:53 +0000

    pull out lineage device_framework_matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,8 +80,7 @@ TARGET_SEC_FP_CALL_NOTIFY_ON_CANCEL := true
 # specifies framework requirements on conforming vendor implementations
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/device_framework_matrix.xml \
-    $(COMMON_PATH)/samsung_device_framework_matrix.xml \
-    $(COMMON_PATH)/aidl_compatibility_matrix.xml
+    $(COMMON_PATH)/lineage_device_framework_matrix.xml
 # specifies which HAL versions the vendor image provides
 DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/manifest.xml
